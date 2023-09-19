f3a381e1b41bb549a5b9e544426236ba622cb825
validador <143583933+validador@users.noreply.github.com>
2023-09-19 14:10:16 -0600

    Add files via upload

---
+++ js/toggle-class.js
@@ -0,0 +1,38 @@
+// *************** INDEX.HTML ***************
+
+// CODE FOR SHOWING BEHAVIOR - INFO SQUARES IN SIGED MAIN PAGE
+
+// DOCENTES*
+const btn2 = document.getElementById('btn2'); 
+btn2.addEventListener('click', () => { 
+// 👇️ hide button 
+btn2.style.visibility = 'hidden'; 
+btn2.style.opacity = 1; 
+// 👇️ show div 
+const box2 = document.getElementById('box2'); 
+box2.style.visibility = 'visible';
+});
+
+// ESCUELAS*
+const btn3 = document.getElementById('btn3'); 
+btn3.addEventListener('click', () => { 
+// 👇️ hide button 
+btn3.style.visibility = 'hidden'; 
+btn3.style.opacity = 1; 
+// 👇️ show div 
+const box3 = document.getElementById('box3'); 
+box3.style.visibility = 'visible';
+});
+
+// DOCUMENTOS*
+const btn4 = document.getElementById('btn4'); 
+btn4.addEventListener('click', () => { 
+// 👇️ hide button 
+btn4.style.visibility = 'hidden'; 
+btn4.style.opacity = 1; 
+// 👇️ show div 
+const box4 = document.getElementById('box4'); 
+box4.style.visibility = 'visible';
+});
+// ----------------------------------------------------------------------------
+
